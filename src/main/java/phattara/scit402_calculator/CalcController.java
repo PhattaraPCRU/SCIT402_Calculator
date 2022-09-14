db5302c299d9469f4028368215fa41d3876c102d
PhattaraPCRU Nitro5 <st641102064111@office365.pcru.ac.th>
2022-09-14 15:52:52 +0700

    first commit

---
+++ src/main/java/phattara/scit402_calculator/CalcController.java
@@ -0,0 +1,70 @@
+package phattara.scit402_calculator;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import javax.swing.JTextField;
+
+public class CalcController {
+    public static void addNumber(String button,JTextField txtf_output) {
+        String display = txtf_output.getText().replace(",", "");
+        if(display.equals("0.00")) {
+            display = "";
+        }
+        switch(button){
+            case ".":
+            case "0":
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if(display.equals("")){
+                    break;
+                }
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                txtf_output.setText(display + button);
+                break;
+            case "=":
+                calculate(txtf_output);
+                break;
+        }
+    }
+    static void calculate(JTextField txtf_ouput){
+        String display = txtf_ouput.getText();
+        BigDecimal number1, number2,total = BigDecimal.ZERO;
+        OUTER:
+        for (int i = 1; i<display.length(); i++) {
+            String charAt = "" + display.charAt(i);
+            switch(charAt){
+                case "+":
+                    number1 = new BigDecimal(display.substring(0, i));
+                    number2 = new BigDecimal(display.substring(i+1, display.length()));
+                    total = number1.add(number2);
+                    break OUTER;
+                case "-":
+                    number1 = new BigDecimal(display.substring(0, i));
+                    number2 = new BigDecimal(display.substring(i+1, display.length()));
+                    total = number1.subtract(number2);
+                    break OUTER;
+                case "*":
+                    number1 = new BigDecimal(display.substring(0, i));
+                    number2 = new BigDecimal(display.substring(i+1, display.length()));
+                    total = number1.multiply(number2);
+                    break OUTER;
+                case "/":
+                    number1 = new BigDecimal(display.substring(0, i));
+                    number2 = new BigDecimal(display.substring(i+1, display.length()));
+                    total = number1.divide(number2, 2, BigDecimal.ROUND_HALF_DOWN);
+                    break OUTER;
+            }
+        }
+        DecimalFormat dec = new DecimalFormat("#,##0.00");
+        txtf_ouput.setText(dec.format(total.doubleValue()));
+    }
+}
